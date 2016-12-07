e7d9fc729847cdc921604318e2dce0eeecb5de2d
Jordi <jfernandez@clever-age.com>
2016-12-07 19:57:19 +0800

    pull.sh

--- pull.sh
+++ pull.sh
@@ -1,10 +1,10 @@
 git pull
-rm -r var/cache var/logs
+sudo rm -r var/cache var/logs
 mkdir var/cache
 mkdir var/logs
-chown -R www-data:www-data app/cache/ app/logs
-chmod -R 775 var/cache/ var/logs
-chmod -R 775 web
+sudo chown -R www-data:www-data var/cache/ var/logs
+sudo chmod -R 775 var/cache/ var/logs
+sudo chmod -R 775 web
 composer install
 app/console assets:install --symlink
 
